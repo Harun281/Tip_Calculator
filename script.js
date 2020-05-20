a1eee79e8286515faecc68c7f9680ceabca2aa57
Harun <61190057+Harun281@users.noreply.github.com>
2020-05-20 16:11:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/*document.getElementById("container").onchange = function(){
-    var bill = Number(document.getElementById("billTotal").value);
-    if(isNaN(bill)){
-        document.getElementById("billError").innerHTML = "Bill can contain numbers only!";
-        document.getElementById("tipOutput").innerHTML = "";
-        document.getElementById("splitOutput").innerHTML = "";
-        document.getElementById("newBill").innerHTML = "$";
-        document.getElementById("tipEach").innerHTML =  "$";
-    }else{
-    var tipPercent = document.getElementById("tipInput").value;
-    var split = document.getElementById("splitInput").value;
-    var tipValue = bill * (tipPercent / 100);
-    var newBillEach = (bill + tipValue) / split;
-    var tipEach = tipValue / split;
-
-    document.getElementById("tipOutput").innerHTML = tipPercent + "%";
-    document.getElementById("splitOutput").innerHTML = split;
-    document.getElementById("newBill").innerHTML = "$" + newBillEach.toFixed(2);
-    document.getElementById("tipEach").innerHTML =  "$" + tipEach.toFixed(2);
-    document.getElementById("billError").innerHTML = "";
-    }
-
-};*/
 document.getElementById("billTotal").onkeyup = function(){
     var bill = Number(document.getElementById("billTotal").value);
     if(isNaN(bill)){
